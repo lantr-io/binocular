2050a78eced43af817b04e66e3235131cbdd10c4
Alexander Nemish <anemish@gmail.com>
2024-12-21 15:20:08 +0100

    chore: update to Scalus 0.8.3

--- project.scala
+++ project.scala
@@ -1,8 +1,8 @@
 //> using scala 3.3.4
 //> using options -deprecation -feature
-//> using plugin org.scalus:scalus-plugin_3:0.8.2+19-18eec7fe-SNAPSHOT
-//> using dep org.scalus:scalus_3:0.8.2+19-18eec7fe-SNAPSHOT
-//> using dep org.scalus:scalus-bloxbean-cardano-client-lib_3:0.8.2+19-18eec7fe-SNAPSHOT
+//> using plugin org.scalus:scalus-plugin_3:0.8.3
+//> using dep org.scalus:scalus_3:0.8.3
+//> using dep org.scalus:scalus-bloxbean-cardano-client-lib_3:0.8.3
 //> using dep com.lihaoyi::upickle:4.0.2
 //> using dep org.slf4j:slf4j-simple:2.0.16
 //> using dep org.bouncycastle:bcprov-jdk18on:1.79
