a553550bbf89a399571fb6f15db4e1d777f6caf8
Ruslan Shevchenko <ruslan@shevchenko.kiev.ua>
2025-11-06 08:17:24 +0200

    added minimal integration tests

---
+++ src/test/scala/binocular/BinocularIntegrationTest.scala
@@ -0,0 +1,84 @@
+package binocular
+
+import com.bloxbean.cardano.client.plutus.spec.PlutusV3Script
+import com.bloxbean.cardano.client.util.HexUtil
+import scalus.builtin.ByteString
+
+import scala.jdk.CollectionConverters.*
+
+/** Integration tests for Binocular Oracle on Yaci DevKit
+  *
+  * These tests require Docker to be running and will start a local Cardano devnet.
+  * Tests are marked with .ignore by default - remove .ignore to run them.
+  *
+  * To run these tests:
+  * 1. Ensure Docker is running
+  * 2. Run: sbt "testOnly *BinocularIntegrationTest"
+  */
+class BinocularIntegrationTest extends YaciDevKitSpec {
+
+    // NOTE: Script compilation commented out for now - requires understanding cardano-client-lib PlutusV3Script API
+    // To implement: compile BitcoinContract.bitcoinProgram to PlutusV3Script
+    // val program = BitcoinContract.bitcoinProgram
+    // val uplc = program.flatEncoded
+    // Create PlutusV3Script from UPLC bytes
+
+    test("Yaci DevKit container starts and has funded account".ignore) {
+        withYaciDevKit(YaciDevKitConfig(enableLogs = true)) { devKit =>
+            // Verify account was funded
+            val balance = devKit.getLovelaceBalance(devKit.account.baseAddress())
+            println(s"Account balance: $balance lovelace")
+
+            assert(balance > 0, "Account should have been funded with initial lovelace")
+            assert(balance >= 100_000_000_000L, "Account should have at least 100,000 ADA")
+        }
+    }
+
+    // NOTE: Commented out - requires implementing PlutusV3Script creation
+    // test("Can create initial script UTXO with genesis state".ignore) { ... }
+
+    test("Can query protocol parameters".ignore) {
+        withYaciDevKit() { devKit =>
+            val paramsResult = TransactionBuilders.getProtocolParamsHelper(devKit.getBackendService)
+
+            paramsResult match {
+                case Right(params) =>
+                    println(s"Protocol parameters:")
+                    println(s"  Min fee A: ${params.minFeeA}")
+                    println(s"  Min fee B: ${params.minFeeB}")
+                    println(s"  Max TX size: ${params.maxTxSize}")
+                    println(s"  Max block header size: ${params.maxBlockHeaderSize}")
+
+                    assert(params.minFeeA > 0, "Min fee A should be positive")
+                    assert(params.minFeeB > 0, "Min fee B should be positive")
+                    assert(params.maxTxSize > 0, "Max TX size should be positive")
+
+                case Left(error) =>
+                    fail(s"Failed to get protocol params: $error")
+            }
+        }
+    }
+
+    /** Helper: Create a genesis ChainState for testing
+      *
+      * Uses Bitcoin block 865493 as the initial confirmed state.
+      */
+    private def createGenesisState(): BitcoinValidator.ChainState = {
+        val genesisHeight = BigInt(865493)
+        val genesisHash = ByteString.fromHex("0000000000000000000143a112c5ab741ec6e95b6c80f9834199efe2154c972b").reverse
+        val genesisTarget = ByteString.fromHex("17030ecd").reverse
+        val genesisTimestamp = BigInt(1736701001)
+        val genesisRecentTimestamps = scalus.prelude.List.single(genesisTimestamp)
+
+        BitcoinValidator.ChainState(
+          blockHeight = genesisHeight,
+          blockHash = genesisHash,
+          currentTarget = genesisTarget,
+          blockTimestamp = genesisTimestamp,
+          recentTimestamps = genesisRecentTimestamps,
+          previousDifficultyAdjustmentTimestamp = genesisTimestamp - 600 * BitcoinValidator.DifficultyAdjustmentInterval,
+          confirmedBlocksRoot = genesisHash,
+          forksTree = scalus.prelude.AssocMap.empty
+        )
+    }
+}
